e4302517c1866aabf0e74a556a0df069f21d74d1
ccolonel37 <ccolonel37@gmail.com>
2022-09-05 10:42:51 -0500

    First program

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 
+
 int main(){
 
-    std::cout << "Number1" << std::endl;
-    std::cout << "Number2" << std::endl;
-    std::cout << "Number3" << std::endl;
+    std::cout << "Connie Hickson" << std::endl;
+    std::cout << "Connie Hickson" << std::endl;
+    std::cout << "Connie Hickson" << std::endl;
+    std::cout << "Connie Hickson" << std::endl;
     return 0;
 }
+
